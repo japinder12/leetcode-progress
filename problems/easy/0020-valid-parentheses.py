2226413b85e25a6e1b22cfc638e7afd60897018e
Japinder <japinder.narula12@gmail.com>
2025-09-04 01:42:22 -0400

    add 0020 Valid Parentheses (Easy)

--- problems/easy/0020-valid-parentheses.py
+++ problems/easy/0020-valid-parentheses.py
@@ -4,5 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
-
+        stack = []
+        pair = {')': '(', ']': '[', '}': '{'}
+        
+        for ch in s:
+            if ch in pair.values():
+                stack.append(ch)
+            elif ch in pair:
+                if not stack or stack[-1] != pair[ch]:
+                    return False
+                stack.pop()
+        return not stack
