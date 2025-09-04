5801ab32c16489ad371db76af2927ca530d4ca37
Japinder <japinder.narula12@gmail.com>
2025-09-04 14:22:28 -0400

    add 0021 Merge Two Sorted Lists (Easy)

--- problems/easy/0021-merge-two-sorted-lists.py
+++ problems/easy/0021-merge-two-sorted-lists.py
@@ -12,14 +12,13 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        if list1 is None:
+        if not list1:
             return list2
-        if list2 is None:
+        if not list2:
             return list1
-        if list1.val <= list2.val:
+        if list1.val < list2.val:
             list1.next = self.mergeTwoLists(list1.next, list2)
             return list1
         else:
             list2.next = self.mergeTwoLists(list1, list2.next)
             return list2
-        
