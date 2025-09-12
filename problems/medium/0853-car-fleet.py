1a5ca4bbb44850bc51305d283198cbfc4b6aa59f
Japinder <japinder.narula12@gmail.com>
2025-09-12 17:50:19 -0400

    add 0853 Car Fleet (Medium)

--- problems/medium/0853-car-fleet.py
+++ problems/medium/0853-car-fleet.py
@@ -6,5 +6,16 @@ class Solution(object):
         :type speed: List[int]
         :rtype: int
         """
-        pass
+        if len(position) == 1 or len(speed) == 1:
+            return 1
+        
+        cars = sorted(zip(position, speed), reverse=True)
+        stk = []
 
+        for pos, spd in cars:
+            time_left = (target-pos) / float(spd)
+            if not stk or time_left > stk[-1]:
+                # only add cars that are slow enough, aka
+                # can't catch up to car ahead (car in stk)
+                stk.append(time_left)
+        return len(stk)
