1df7a1469e923919fad1366e68f6553636abeaa0
Japinder <japinder.narula12@gmail.com>
2025-09-12 18:45:09 -0400

    add 0084 Largest Rectangle in Histogram (Hard)

--- problems/hard/0084-largest-rectangle-in-histogram.py
+++ problems/hard/0084-largest-rectangle-in-histogram.py
@@ -4,5 +4,19 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        pass
-
+        hs = heights + [0]
+        n = len(hs)
+        stk = []
+        max_area = 0
+        for i in range(n): 
+            curr = hs[i]
+            while stk and curr < hs[stk[-1]]:
+                temp = stk.pop()
+                temp_h = hs[temp]
+                if not stk:
+                    width = i
+                else:
+                    width = i - stk[-1] - 1
+                max_area = max(max_area, width * temp_h)
+            stk.append(i)
+        return max_area
