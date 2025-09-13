47489b48dcc770c02f72ea8b5c263e46478ebce5
Japinder <japinder.narula12@gmail.com>
2025-09-13 17:58:11 -0400

    add 0070 Climbing Stairs (Easy)

--- problems/easy/0070-climbing-stairs.py
+++ problems/easy/0070-climbing-stairs.py
@@ -4,9 +4,23 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n <= 2:
-            return n
-        a, b = 1, 2
-        for _ in range(3, n + 1):
-            a, b = b, a + b
-        return b
+        # if n <= 2:
+        #     return n
+        # a, b = 1, 2
+        # for _ in range(3, n + 1):
+        #     a, b = b, a + b
+        # return b
+
+        dp = []
+
+        for i in range(0, n+1):
+            dp.append(-1)
+        
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+    
