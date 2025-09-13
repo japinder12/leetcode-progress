532d1bbfcd2576b99b3d4a2e2576eff657e280b0
Japinder <japinder.narula12@gmail.com>
2025-09-13 17:50:33 -0400

    add 0064 Minimum Path Say (Medium)

--- problems/medium/0064-minimum-path-sum.py
+++ problems/medium/0064-minimum-path-sum.py
@@ -4,16 +4,16 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        if not grid or not grid[0]:
-            return 0
         m, n = len(grid), len(grid[0])
-        # In-place DP to save space
-        for i in range(m):
-            for j in range(n):
-                if i == 0 and j == 0:
-                    continue
-                top = grid[i-1][j] if i > 0 else float('inf')
-                left = grid[i][j-1] if j > 0 else float('inf')
-                grid[i][j] += min(top, left)
-        return grid[-1][-1]
+        
+        for i in range(1, n):
+            grid[0][i] += grid[0][i-1]
+        for i in range(1, m):
+            grid[i][0] += grid[i-1][0]
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
 
+        return grid[-1][-1]
+    
