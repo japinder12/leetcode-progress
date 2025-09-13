6562e043825fd60bf0fe3554cc6faf0f034ff587
Japinder <japinder.narula12@gmail.com>
2025-09-13 17:46:29 -0400

    add 0058 Length of Last Word (Easy)

--- problems/easy/0058-length-of-last-word.py
+++ problems/easy/0058-length-of-last-word.py
@@ -4,15 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        i = len(s) - 1
-        # skip trailing spaces
-        while i >= 0 and s[i] == ' ':
-            i -= 1
-        if i < 0:
-            return 0
-        # count last word
-        j = i
-        while j >= 0 and s[j] != ' ':
-            j -= 1
-        return i - j
-
+        s = s.strip()
+        words = s.split(" ")
+        return len(words[-1])
